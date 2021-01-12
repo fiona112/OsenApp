fabde37e94e83ee2438e7374b1d3d2f0283a6d0c
Dragon He <dragon@abstractultra.com>
2021-01-12 10:28:07 -0500

    dashboard

--- packages/mobile/src/screens/DashboardScreen.js
+++ packages/mobile/src/screens/DashboardScreen.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { View } from "react-native";
 import { Header, Text } from "react-native-elements";
-import { StatusBar } from "expo-status-bar";
 import { Surface } from "react-native-paper";
+import { ProgressChart } from "react-native-chart-kit";
 
-function DashboardCard({ children }) {
+function DashboardCard({ children, title }) {
   return (
     <Surface
       style={{
@@ -16,24 +16,73 @@ function DashboardCard({ children }) {
       }}
       elevation={4}
     >
+      <View style={{ padding: 10 }}>
+        <Text h5 style={{ textAlign: "center" }}>
+          {title}
+        </Text>
+      </View>
       {children}
     </Surface>
   );
 }
 
+function StatisticCard() {
+  const chartConfig = {
+    backgroundGradientFrom: "#FFFFFF",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#FFFFFF",
+    backgroundGradientToOpacity: 0.0,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false, // optional
+  };
+  return (
+    <DashboardCard title="WALKING CHALLENGE">
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: 100,
+        }}
+      >
+        <View style={{ position: "absolute" }}>
+          <Text>76%</Text>
+        </View>
+        <View style={{ position: "absolute" }}>
+          <ProgressChart
+            height={220}
+            width={220}
+            strokeWidth={16}
+            data={{ data: [0.7] }}
+            hideLegend={true}
+            chartConfig={chartConfig}
+            background
+          />
+        </View>
+      </View>
+    </DashboardCard>
+  );
+}
+
 export default function DashboardScreen() {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Header
         centerComponent={{ text: "DASHBOARD", style: { color: "#fff" } }}
       />
-      <View style={{ flexDirection: "row", padding: 20 }}>
-        <DashboardCard>
-          <Text h3>240{"\n"}steps</Text>
-        </DashboardCard>
-        <DashboardCard>
-          <Text h3>stuff</Text>
-        </DashboardCard>
+      <View style={{ flexDirection: "row", flex: 1, padding: 20 }}>
+        <View style={{ flexDirection: "column", flex: 1 }}>
+          <StatisticCard />
+          <StatisticCard />
+        </View>
+        <View style={{ flexDirection: "column", flex: 1 }}>
+					<DashboardCard title="LOCAL LEADERS">
+            <Text>
+              1. John Doe
+            </Text>
+          </DashboardCard>
+        </View>
       </View>
     </View>
   );
